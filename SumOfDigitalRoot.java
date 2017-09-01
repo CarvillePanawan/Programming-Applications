72b25048da09a3e94cacb6a85337b7eba5784c87
Zukato999 <31525596+Zukato999@users.noreply.github.com>
2017-09-01 10:10:38 +0800

    Update SumOfDigitalRoot.java

--- SumOfDigitalRoot.java
+++ SumOfDigitalRoot.java
@@ -16,17 +16,18 @@ public class SumOfDigitalRoot {
   
   /**
    *Algorithm:
-   *Let sum be an integer initialized to 0
-   *while number is greater than 9
-   *	Let sum be equal to 0
-   *	while number is greater than 0
-   *		Let remainder be an integer
-   *		Let remainder be equal to number% 10
-   *		Let sum be equal to sum + remainder
-   *		Let number be equal to number/10
-   *	Let number be equal to sum
-   *return sum
+   *1.Declare int sum, that will be used to store the sum of digital roots
+   *2.while number is greater than 9
+   *2.1.Let sum be equal to 0
+   *2.2.while number is greater than 0
+   *2.2.1.Let remainder be an integer
+   *2.2.2.Let remainder be equal to number% 10
+   *2.2.3.Let sum be equal to sum + remainder
+   *2.2.4.Let number be equal to number/10
+   *2.3.Let number be equal to sum
+   *3.return sum
    *@param
+   *@return the sum of digital roots.
    */
   public static int digital_root_sum(int number) {
     int sum=0;
