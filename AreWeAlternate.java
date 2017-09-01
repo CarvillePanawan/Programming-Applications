3b1b4f1ea17338185026244f65e4b86bc0c85b05
gerigd <31525850+gerigd@users.noreply.github.com>
2017-09-01 18:44:02 +0800

    Add files via upload

--- AreWeAlternate.java
+++ AreWeAlternate.java
@@ -1,37 +1,51 @@
+/*
+Solution for "Are we alternate?"
+Algorithm:
+    1. Creates an array of vowels
+    2. Declare boolean variables (vowelP, vowelC, isAlternating) 
+       vowelP = stands for vowel previous, checks if the first character of the string is in the array
+       vowelC = stands for vowel current, checks if the second character of the string is in the array
+       isAlternating = confirms if its an alternating vowel and consonant or consonant and vowel
+    3. Add a for loop to check each letter of the given string
+    4. Add another for loop inside the first for loop to check each indexes of the array
+        4.1 If the first character of the string is in the array
+        4.2 Then, vowelP is true
+        4.3 Also add an if statement to check if the second character of the string is in the array,.
+        4.3  Then, vowelC is true.
+    5. If Vowel previous and Vowel current are not equal, then it is alternating
+    6. Vowel previous and vowel current must be equal since the latter indicates that the
+       letter is a consonant and is not found in the array.
+      
+*/
+import java.util.*;
+public class AreWeAlternate {
 
-
-	/**
-	 * Solution for Are we alternate? 
-	 * Create a function isAlt() that accepts a string as an argument and validates 
-	 * whether the vowels (a, e, i, o, u) and consonants are in alternate order.
-	 * 
-	 * ALGORITHM:
-	 * 1. Given a string from a user input
-	 * 2. Check if the first char is a vowel using the isVowel method
-	 * 3. Using a for loop determine if the letters in the word are in alternate vowels
-	 * 4. If true, return true else false
-	 * 
-	 *  @param word the user input
-	 *  @return boolean value if its alternate or not
-	 */
-	public static boolean isAlt(String word) 
-    {
-      boolean b = isVowel(word.charAt(0));
-  		for (int i = 1; i < word.length(); i++)
-  		{
-  			b = !b;
-  			if (b != isVowel(word.charAt(i))) return false;
-  		}
-  		return true;	
+    public static boolean isAlt(String word) {
+        char [] vowels = {'a','e','i','o','u'};  
+     	boolean vowelP = false;
+     	boolean vowelC = false;
+     	boolean isAlternating=false;
+    	
+    	for(int x = 0; x <word.length()-1; x++){
+            for(int y = 0; y<vowels.length; y++){
+            	if(x==0 && word.charAt(x)==vowels[y]){
+            		vowelP = true;
+                    }
+            	if(word.charAt(x+1)==vowels[y]){
+            		vowelC = true;
+                    }
+           	}
+           	if(vowelP!=vowelC){
+           		isAlternating=true;
+                    }
+           	vowelP=vowelC;
+             }
+        
+        return isAlternating;
     }
-     static boolean isVowel(char ch) 
-    {
-	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-	{
-		return true;
-	}
 
-        return false;
-
+    public static void main(String[]args){
+        AreWeAlternate  s = new AreWeAlternate();
+        System.out.println(s.isAlt("banana"));
     }
-
+}
