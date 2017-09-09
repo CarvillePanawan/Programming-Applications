aee556f31e54771d4e4996fe6c7ef309b069f0ad
Zukato999 <31525596+Zukato999@users.noreply.github.com>
2017-09-09 23:43:36 +0800

    Add files via upload

---
+++ Unit Testing/Apache Ant/toSmallestTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ToSmallestTest {
+
+    private static void testing(long n, String res) {
+        assertEquals(res, Arrays.toString(toSmallest.smallest(n)));
+    }
+    @Test
+    public void test() {
+        testing(261235, "[126235, 2, 0]");
+        testing(209917, "[29917, 0, 1]");
+        testing(285365, "[238565, 3, 1]");
+        testing(269045, "[26945, 3, 0]");
+        testing(296837, "[239687, 4, 1]");
+    }
+}
