57b9956988613d0aee578f69a1b81a7b7bccf076
gerigd <31525850+gerigd@users.noreply.github.com>
2017-09-11 22:19:21 +0800

    Update FlamesTest.java

--- Unit Testing/Maven/FlamesTest.java
+++ Unit Testing/Maven/FlamesTest.java
@@ -17,4 +17,25 @@ public class FlamesTest {
     assertEquals("NEIL and MAE are affection" ,
         "Affection", Flames.showRelationship("JOSHUA", "CIELO"));
   }
+  public void test4 {
+    assertEquals("EDWARD and POTPOT are siblings" ,
+        "Siblings", Flames.showRelationship("EDWARD", "POTPOT"));
+ 	 }
+  public void test5{
+    assertEquals("BRYANT and CHARLOTTE are love" ,
+        "Love", Flames.showRelationship("BRYANT", "CHARLOTTE"));
+  	}
+  public void test6{
+    assertEquals("RAPHA and KENNETH are marriage" ,
+        "Marriage", Flames.showRelationship("RAPHA","KENNETH"));
+  	}
+  public void test7{
+    assertEquals("ABCDEFGHIJKL and MNOPQRSTUVWXYZ are love" ,
+        "Love", Flames.showRelationship("ABCDEFGHIJKL", " MNOPQRSTUVWXYZ"));
+ 	}
+  public void test8{
+    assertEquals("AAABBCC and ABCD are friendship" ,
+        "Friendship", Flames.showRelationship("AAABBCC", " ABCD"));
+ 	}
+ 	
 }
